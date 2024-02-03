642334b93ad493cf8f3aee18f57562797298b6e9
qabil amanullayev <qabil.amanullayev@gmail.com>
2024-02-03 23:41:38 +0400

    day3

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,57 @@ function hideMenuContainer() {
 
 navMenu.addEventListener('click', showMenuContainer);
 popupClose.addEventListener('click', hideMenuContainer);
+
+let arr = [];
+let page=1;
+let sec4Container = document.querySelector('.sec4-container');
+let activeCategory = "all"; // default category
+
+function showJobs(category) {
+    sec4Container.innerHTML = "";
+
+    arr.forEach(element => {
+        const elementCategory = element.category.toLowerCase();
+
+        if (category === "all" || elementCategory.includes(category.toLowerCase())) {
+            sec4Container.innerHTML += `
+                <div class="sec4-box">
+                    <img src="${element.jobImage}" alt="Image">
+                </div>
+                <div class="rec-box-p">${element.job}</div>
+                <div class="rec-box-p">${element.name}</div>
+                <div class="rec-box-p">${element.password}</div>
+                <div class="rec-box-p">${element.gmail}</div>
+                <div class="rec-box-p">${element.id}</div>
+            `;
+        }
+    });
+}
+
+function handlePaginationClick(event) {
+    event.preventDefault(); // Prevent the default behavior of the clickable element
+
+    const clickedCategory = event.currentTarget.getAttribute("data-category");
+
+    document.querySelector('.sec4-pagination .active').classList.remove('active');
+    event.currentTarget.classList.add('active');
+
+    showJobs(clickedCategory);
+
+    activeCategory = clickedCategory;
+}
+
+document.querySelectorAll('.sec4-pagination div').forEach(paginationItem => {
+    paginationItem.addEventListener('click', handlePaginationClick);
+});
+
+function getDataJson() {
+    fetch(`http://localhost:3000/jobs?_page=${page}&_limit=12`)
+        .then(response => response.json())
+        .then(data => {
+            arr = data;
+            showJobs(activeCategory);
+        });
+}
+
+getDataJson();
